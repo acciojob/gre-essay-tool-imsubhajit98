b5e5b1c4813fa8859019cbde4e21f54a00b420e8
Subhajit Guchhait <74090703+imsubhajit98@users.noreply.github.com>
2023-01-20 17:53:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,13 +4,19 @@ let header=document.getElementById('wordCount');
 text.addEventListener('keyup',function(){
 	let sentence=text.value;
 
-	let count=0;
+	let count=0, flag=false;
 	for(let i=0;i<sentence.length;i++){
 		if(sentence[i]==" "){
 			count=count+1;
 		}
+		if(sentence[sentence.length-1]==" "){
+			flag=true;
+		}
 	}
-	count=count+1;
+	if(flag==false){
+		count=count+1;
+	}
+	
 	
 	header.innerText=count;
 });
