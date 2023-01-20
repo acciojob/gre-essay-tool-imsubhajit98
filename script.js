221a343d635f0fd962337b75f52862117d85e7b4
Subhajit Guchhait <74090703+imsubhajit98@users.noreply.github.com>
2023-01-20 17:12:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 //your code here
+let text=document.getElementById('evaluatedText');
+let header=document.getElementById('wordCount');
+text.addEventListener('keyup',function(){
+	header.innerText=text.value.length;
+});
